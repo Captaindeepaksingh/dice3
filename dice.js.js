1091ae0f666a04822c01122be798ec07f130b1f5
Captaindeepaksingh <97876669+Captaindeepaksingh@users.noreply.github.com>
2022-02-17 07:56:41 -0500

    Add files via upload

---
+++ dice.js.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor((Math.random()*6)) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+ var image1 = document.querySelectorAll("img")[0];
+ image1.setAttribute("src" , randomImageSource);
+
+ var randomNumber2 = Math.floor((Math.random()*6)) + 1;
+ var randomDiceImage1 = "dice" + randomNumber2 + ".png";
+ var randomImageSource1 = "images/" + randomDiceImage1;
+ var image2 = document.querySelectorAll("img")[1];
+ image2.setAttribute("src" , randomImageSource1);
+   
+ if(randomNumber1 > randomNumber2){
+     document.querySelector("h1").innerHTML = " 🚩player1 wins!";
+ }
+ else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "player2 wins! 🚩";
+ }
+ else{
+    document.querySelector("h1").innerHTML = "Draw!";
+ }
